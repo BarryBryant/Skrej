84fab2f735ab0a93971f321de97e8017d11981a0
Barry Bryant <barrybryant@WTA-MBP226.local>
2016-11-11 14:15:51 -0500

    resolve merge conflicts

--- app/src/main/java/com/willowtreeapps/skrej/CalendarLoader.java
+++ app/src/main/java/com/willowtreeapps/skrej/CalendarLoader.java
@@ -14,50 +14,26 @@ import com.google.api.services.calendar.model.Events;
 import java.io.IOException;
 import java.util.List;
 
+import javax.inject.Inject;
+
 /**
  * Created by barrybryant on 11/8/16.
  */
 
 public class CalendarLoader extends EricRichardsonLoader<List<Event>> {
 
-    private static final String CACTUAR_ID = "willowtreeapps.com_3632363436343537393337@resource.calendar.google.com";
-    private static final String DEKU_ID = "willowtreeapps.com_2d3531383336393730383033@resource.calendar.google.com";
-    private static final String SUDOWOODO_ID = "willowtreeapps.com_2d3331363639303230383838@resource.calendar.google.com";
-    private static final String ELDERBERRY_ID = "willowtreeapps.com_2d3839383537323139333730@resource.calendar.google.com";
+    @Inject
+    CredentialHelper credentialHelper;
 
-
-    private GoogleAccountCredential credential;
     private com.google.api.services.calendar.Calendar service;
     private Exception lastError;
     private String roomId;
 
-    public CalendarLoader(Context context, GoogleAccountCredential credential, String roomId) {
+    public CalendarLoader(Context context, String roomId) {
         super(context);
-        this.credential = credential;
-
-
-        switch(roomId) {
-            case "Cactuar":
-                this.roomId = CACTUAR_ID;
-                break;
-            case "Deku":
-                this.roomId = DEKU_ID;
-                break;
-            case "Sudowoodo":
-                this.roomId = SUDOWOODO_ID;
-                break;
-            case "Elderberry":
-            default:
-                this.roomId = ELDERBERRY_ID;
-                break;
-        }
-
-        HttpTransport transport = AndroidHttp.newCompatibleTransport();
-        JsonFactory jsonFactory = JacksonFactory.getDefaultInstance();
-        service = new com.google.api.services.calendar.Calendar.Builder(
-                transport, jsonFactory, credential)
-                .setApplicationName("Google Calendar API Android Quickstart")
-                .build();
+        ConferenceApplication.get(context.getApplicationContext()).component().inject(this);
+        this.roomId = roomId;
+        service = credentialHelper.getCalendarService();
     }
 
     @Override
